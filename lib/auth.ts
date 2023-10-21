9e67980c7fceb58245ab32541e318a0f47880be8
Haikhal Fakhreez <haikhalfakhreez@gmail.com>
2023-10-22 06:35:28 +0800

    update auth config

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from "next-auth"
+import { NextAuthOptions, User } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: NextAuthOptions = {
@@ -22,4 +22,48 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    async jwt({ token, user }) {
+      // Initial sign in
+      if (user) {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          picture: user.image,
+        }
+      }
+
+      return {
+        id: token.id,
+        name: token.name,
+        email: token.email,
+        picture: token.picture,
+      }
+    },
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+
+      return session
+    },
+  },
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      id: string
+    }
+  }
 }
